4e29a0c08dacf2c70eff78d3ce39c5f26f0877db
goddard-guryon <neaviduox1295@outlook.com>
2022-09-13 22:41:26 +0530

    added simulation...kinda; its still buggy

--- maze.py
+++ maze.py
@@ -2,40 +2,66 @@
 Create a 2D maze
 """
 from random import uniform, choice
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
 
-def make_maze(m, n=0):
-    # instantiate variables
-    check = 0
-    nodes, edges = {}, []
-    if not n:
-        n = m
+def fix_path(n, is_in_path, conns):
+    """
+    Fix path on grid to add any missing nodes
+    """
+    while not all(is_in_path.values()):
+        strays = [k for k, v in is_in_path.items() if not v]
+        for cur in strays:
 
-    # first set up nodes
-    for i in range(m+1):
-        nodes[i] = {}
-        for j in range(n+1):
-            nodes[i][j] = (i, j)
+            # maybe we already passed through this box in an
+            # earlier iteration of this same loop
+            if not is_in_path[cur]:
+                connected = False
+                c = cur
+                while not connected:
+                    nbrs = [c+1, c-1, c+n, c-n]
+                    nbrs = [x for x in nbrs if x in is_in_path]
+                    if not c%n:
+                        nbrs.remove(c+1)
+                    elif c%n == 1:
+                        nbrs.remove(c-1)
+                    pick = choice(nbrs)
+                    conns[c].append(pick)
+                    conns[pick].append(c)
+                    is_in_path[c] = True
+                    if is_in_path[pick]:
+                        connected = True
+                    else:
+                        c = pick
+    return conns
 
-    # create a path through the maze first
+
+def create_path(m, n):
+    """
+    Create a path through a grid
+    """
     is_in_path = {x: False for x in range(1, m*n+1)}
     conns = {x: [] for x in is_in_path}
     current = [1]
     is_in_path[1] = True
+    check = 0
 
     # create a main path
     while current:
         check += 1
         cur = current.pop()
         nbrs = [cur+1, cur-1, cur+n, cur-n]
-        if not (cur+1)%n:
+
+        # if you're at the edge of the grid, don't fall off
+        if not cur%n:
             nbrs.remove(cur+1)
-        if not (cur-1)%n:
+        elif cur%n == 1:
             nbrs.remove(cur-1)
         nbrs = [x for x in nbrs if x in is_in_path]
+
+        # since we don't want to go in every possible direction
         do_more = uniform(0, 1)
-        while do_more < .9 and nbrs:
+        while do_more < .99 and nbrs:
             nxt = choice(nbrs)
             nbrs.remove(nxt)
             if not is_in_path[nxt]:
@@ -45,23 +71,33 @@ def make_maze(m, n=0):
                 is_in_path[cur] = True
                 current.append(nxt)
             do_more = uniform(0, 1)
+
+        # in case we run into infinite loop, run the function again
         if check > 1e4:
-            return 1
+            return create_path(m, n)
 
     # then add any missed boxes to path
-    while not all(is_in_path.values()):
-        strays = [k for k, v in is_in_path.items() if not v]
-        for cur in strays:
-            nbrs = [cur+1, cur-1, cur+n, cur-n]
-            nbrs = [x for x in nbrs if x in is_in_path]
-            if not (cur+1)%n:
-                nbrs.remove(cur+1)
-            if not (cur-1)%n:
-                nbrs.remove(cur-1)
-            pick = choice(nbrs)
-            conns[cur].append(pick)
-            conns[pick].append(cur)
-            is_in_path[cur] = True
+    conns = fix_path(n, is_in_path, conns)
+
+    # that's it
+    return conns
+
+
+def maze_grid(m, n):
+    """
+    Create 2D grid
+    """
+    # instantiate variables
+    nodes, edges = {}, []
+
+    # first set up nodes
+    for i in range(m+1):
+        nodes[i] = {}
+        for j in range(n+1):
+            nodes[i][j] = (i, j)
+
+    # create path through the maze
+    conns = create_path(m, n)
 
     # add edges at the border of the maze
     for i in range(m):
@@ -72,30 +108,43 @@ def make_maze(m, n=0):
         edges.append((nodes[m][i], nodes[m][i+1]))
 
     # now add edges so as to preserve path
-    for b in range(1, m*n):
+    for b in range(1, m*n+1):
         i, j = b//n+1, b%n
         if not j:
             j = n
+            i -= 1
         box = {b-n: (nodes[i-1][j-1], nodes[i-1][j]),
                b-1: (nodes[i-1][j-1], nodes[i][j-1]),
                b+1: (nodes[i-1][j], nodes[i][j]),
                b+n: (nodes[i][j-1], nodes[i][j])}
-        if i == n:
-            del box[b+n]
         for nbr, wall in box.items():
             if nbr not in conns[b]:
                 edges.append(wall)
 
-    # idk why, but the top row of maze is messing up
-    for i in range(1, n):
-        box = {"left": (nodes[i-1][n-1], nodes[i-1][n]),
-               "down": (nodes[i-1][n-1], nodes[i][n-1]),
-               "right": (nodes[i][n-1], nodes[i][n]),
-               "up": (nodes[i-1][n-1], nodes[i][n])}
-        if box["left"] in edges and box["right"] in edges and box["down"] in edges:
-            edges.remove(box["down"])
-        elif box["down"] in edges and uniform(0, 1) < .5:
-            edges.remove(box["down"])
-
     # done
+    return edges
+
+
+def make_maze(m, n=0):
+    """
+    Main function to make 2D maze
+    """
+    # all that's left is adding an entry and exit point
+    if not n:
+        n = m
+    edges = maze_grid(m, n)
+
+    # entry point
+    edges.append(((0, m), (0, m+1)))
+    edges.append(((0, m+1), (1, m+1)))
+    edges.append(((1, m+1), (1, m)))
+    edges.append(((0, m+1), (0, m)))
+    edges.append(((1, m+1), (0, m+1)))
+    edges.append(((1, m), (1, m+1)))
+
+    # make way
+    for rem in [((0, m), (1, m)), ((1, m), (0, m)), ((n-1, 0), (n, 0)), ((n, 0), (n-1, 0))]:
+        while rem in edges:
+            edges.remove(rem)
+
     return edges
--- simulate.py
+++ simulate.py
@@ -1,42 +1,211 @@
 """
 EdMD simulation
 """
+import os
+from math import sqrt
+from random import uniform
 from maze import make_maze
 import matplotlib.pyplot as plt
+import cv2
 
 
-def wall_time(x, v, r):
+def get_sg_pr(n):
+    """
+    Create wall and pair permutations of disks
+    """
+    walls = [(a, b) for a in range(n) for b in range(2)]
+    pairs = [(a, b) for a in range(n) for b in range(a+1, n)]
+    return walls, pairs
+
+
+def wall_time(pos, v, r, k, l, edges):
     """
     Time before a particle hits a wall
     """
-    # maybe find a value k, which is the number
-    # of blocks which are empty between disk and next wall.
-    # eg: if only empty blocks for next 3 blocks, then a wall,
-    # then k=3. Then return (k-r-x)/v
-    if v > 0:
-        return (1-r-x)/v
-    if v == 0:
-        return (x-r)/abs(v)
+    x = pos[k][l]
+    i, j = [int(a) for a in pos[k]]
+    if not l:
+        if v > 0:
+            loc = [((i+1, j), (i+1, j+1)), ((i+1, j+1), (i+1, j))]
+        else:
+            loc = [((i, j), (i, j+1)), ((i, j+1), (i, j))]
+    else:
+        if v > 0:
+            loc = [((i, j+1), (i+1, j+1)), ((i+1, j+1), (i, j+1))]
+        else:
+            loc = [((i, j), (i+1, j)), ((i+1, j), (i, j))]
+    if any(a in edges for a in loc):
+        if v > 0:
+            return (-(x%(-1))-r)/v
+        if v < 0:
+            return ((x%1)-r)/abs(v)
     return float("inf")
 
 
-def get_next_event(pos, vel, singles, s):
+def pair_time(x_a, v_a, x_b, v_b, r):
+    """
+    Time before two particles hit each other
+    """
+    (dx, dx_2), (dv, dv_2) = map(lambda a, b: ([b[0]-a[0], b[1]-a[1]],
+                                               (b[0]-a[0])**2 + (b[1]-a[1])**2),
+                                 (x_a, v_a), (x_b, v_b))
+    b_ij = dv[0]*dx[0] + dv[1]*dx[1]
+    upsilon = b_ij**2 - dv_2*(dx_2 - 4*r**2)
+    return -(b_ij + sqrt(upsilon))/dv_2 if b_ij < 0 < upsilon else float("inf")
+
+
+def get_next_event(p, v, singles, pairs, s, edges):
     """
     Find which event happens next
     """
-    hits = [wall_time(pos[k][l], vel[k][l], s) for k, l in singles]
-    return min(zip(hits, range(len(hits))))
+    walls = [wall_time(p, v[k][l], s, k, l, edges) for k, l in singles]
+    pairs = [pair_time(p[k], v[k], p[l], v[l], s) for k, l in pairs]
+    return min(zip(walls+pairs, range(len(walls+pairs))))
 
 
-def get_velocities(pos, vel, singles, n_ix, walls):
+def get_velocities(pos, vel, singles, pairs, n_ix, edges):
     """
     Change velocities after event
     """
-    disk, direction = singles[n_ix]
+    if n_ix < len(singles):
+        disk, direction = singles[n_ix]
+        vel[disk][direction] *= -1
+    else:
+        a, b = pairs[n_ix - len(singles)]
+        dx = [pos[b][0]-pos[a][0], pos[b][1]-pos[a][1]]
+        x_ij = sqrt(dx[0]**2 + dx[1]**2)
+        x_ij = [q/x_ij for q in dx]
+
+        dv = [vel[b][0]-vel[a][0], vel[b][1]-vel[a][1]]
+        b_ij = dv[0]*x_ij[0] + dv[1]*x_ij[1]
+        for k in range(2):
+            vel[a][k] += x_ij[k]*b_ij
+            vel[b][k] -= x_ij[k]*b_ij
+    return vel
 
 
-grid_size = 19
-edges = make_maze(grid_size)
-for edge in edges:
-    plt.plot([edge[0][0], edge[1][0]], [edge[0][1], edge[1][1]], "b-")
-plt.show()
+def fix_delta(pos, vel, r, edges, l, epsilon=1e-2):
+    """
+    Basically bug fixes:
+    - there may be disks that didn't collide with wall,
+      and are now overlapping with it; bounce em back
+    - disks tend to stick to walls if they hit at certain
+      angles; unstick them
+    """
+    x, y = pos
+    i, j = [int(a) for a in pos]
+    if not l:
+        if x < i+1 < x+r:
+            loc = [((i+1, j), (i+1, j+1)), ((i+1, j+1), (i+1, j))]
+            if any(a in edges for a in loc):
+                vel[l] *= -1
+        elif x-r < i < x:
+            loc = [((i, j), (i, j+1)), ((i, j+1),(i, j))]
+            if any(a in edges for a in loc):
+                vel[l] *= -1
+    else:
+        if y < j+1 < y+r:
+            loc = [((i, j+1), (i+1, j+1)), ((i+1, j+1), (i, j+1))]
+            if any(a in edges for a in loc):
+                vel[l] *= -1
+        elif y-r < j < y:
+            loc = [((i, j), (i+1, j)), ((i+1, j), (i, j))]
+            if any(a in edges for a in loc):
+                vel[l] *= -1
+
+    # not working, find some other solution
+    if abs(pos[l]-int(pos[l])) <= epsilon:
+        pos[l] += uniform(-epsilon, epsilon)
+    return pos[l], vel[l]
+
+
+def save_img(i, p, v, r, edges, output_dir, arrow_scale=.2):
+    """
+    Save a snapshot of simulation
+    """
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    plt.clf()
+    for e in edges:
+        plt.plot([e[0][0], e[1][0]], [e[0][1], e[1][1]], "b-")
+    for (x, y), (dx, dy) in zip(p, v):
+        dx *= arrow_scale
+        dy *= arrow_scale
+        circle = plt.Circle((x, y), radius=r)
+        plt.gca().add_patch(circle)
+        plt.arrow(x, y, dx, dy, fc='k', ec='k', head_length=.05, head_width=.05)
+    plt.savefig(os.path.join(output_dir, f"{i}.png"))
+    return 0
+
+
+def run_simulation(n, p, v, r, edges, n_events, dt=5e-5, out="simulation_snapshots"):
+    """
+    Run Molecular Dynamics simulation
+    """
+    # for debugging
+    roof = int(p[0][1])+1
+
+    sg, pr = get_sg_pr(n)
+
+    if os.path.isdir(out):
+        print("Output directory already exists, deleting any files within it...")
+        for file in os.listdir(out):
+            os.remove(f"{out}/{file}")
+    t, i = 0, 0
+
+    next_event, next_event_ix = get_next_event(p, v, sg, pr, r, edges)
+    save_img(i, p, v, r, edges, out)
+    i += 1
+    for _ in range(n_events):
+        if dt:
+            next_t = t + dt
+        else:
+            next_t = t + next_event
+
+        while t+next_event <= next_t:
+            step = min(dt, next_event)
+            t += step
+            for k, l in sg:
+                p[k][l] += v[k][l]*step/2
+                p[k][l], v[k][l] = fix_delta(p[k], v[k], r, edges, l)
+                p[k][l] += v[k][l]*step/2
+            v = get_velocities(p, v, sg, pr, next_event_ix, edges)
+            next_event, next_event_ix = get_next_event(p, v, sg, pr, r, edges)
+        remain_t = next_t - t
+        for k, l in sg:
+            p[k][l] += v[k][l]*remain_t/2
+            p[k][l], v[k][l] = fix_delta(p[k], v[k], r, edges, l)
+            p[k][l] += v[k][l]*remain_t/2
+        t += remain_t
+        next_event -= remain_t
+
+        if not i%2000:
+            save_img(i//2000, p, v, r, edges, out)
+        i += 1
+        print(f"\033[KSimulating timestep {t:.5f} s\r", end='', flush=True)
+
+        # code is still buggy; halt if disks break out of maze
+        for a in p:
+            if a[0]+r < 0 or a[0]-r > roof or a[1]-r > roof or (a[1]+r < 0 and roof-1<a[0]<roof):
+                save_img(i, p, v, r, edges, out)
+                print("Bugged run; halting")
+                return 1
+
+    # convert all snapshots to video
+    #print("\nConverting simulation data to video")
+    #imgs = sorted([f for f in os.listdir(out) if f.endswith(".png")],
+    #              key=lambda x: int(x.split('.')[0]))
+    #height, width, _ = cv2.imread(f"{out}/{imgs[0]}").shape
+    #video = cv2.VideoWriter(f"{out}/animation.avi", 0, 20, (width, height))
+    #for img in imgs:
+    #    video.write(cv2.imread(f"{out}/{img}"))
+    #video.release()
+    print("Done")
+    return 0
+
+
+radius = .1
+positions = [[0.25, 10.25], [0.75, 10.25], [0.25, 10.75], [0.75, 10.75]]
+velocities = [[0.21, 0.12], [0.71, 0.18], [-.43, -.59], [0.78, 0.1177]]
+grid = make_maze(10)
+run_simulation(4, positions, velocities, radius, grid, 1000000)
